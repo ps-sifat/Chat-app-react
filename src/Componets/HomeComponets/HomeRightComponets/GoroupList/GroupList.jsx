f0e0878903449cfdc73a0df4026ac10d1539a66c
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-07-18 00:22:08 +0600

    group list done

--- src/Componets/HomeComponets/HomeRightComponets/GoroupList/GroupList.jsx
+++ src/Componets/HomeComponets/HomeRightComponets/GoroupList/GroupList.jsx
@@ -15,205 +15,34 @@ const GroupList = () => {
             <BsThreeDotsVertical className="text-xl text-primery_Blue" />
           </div>
           {/* title  */}
-          <div className="mt-4 overflow-y-scroll  scrollbar-thumb-sky-700 scrollbar-track-sky-300 scrollbar-thin h-[85%]  gap-y-5 flex flex-col">
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
+          <div className="mt-4 overflow-y-scroll  scrollbar-thumb-primery_Blue scrollbar-track-sky-200 scrollbar-thin h-[85%]  gap-y-5 flex flex-col pr-2">
+            {[...new Array(10)].map((_, index) => (
+              <div
+                key={""}
+                className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] "
+              >
+                <div className="flex  ">
+                  <div className="w-[70px] ">
+                    <picture>
+                      <img src={avater} alt={avater} />
+                    </picture>
+                  </div>
+                  <div className="flex justify-center flex-col ml-[14px] ">
+                    <h3 className="font-custom_popins font-semibold text-lg text-black">
+                      Friends Reunion
+                    </h3>
+                    <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
+                      Hi Guys, Wassup!
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
+                    Join
+                  </button>
                 </div>
               </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
-            {/* profile div  */}
-            <div className="flex justify-between items-center pb-[13px] border-b-2 border-[#00000033] ">
-              <div className="flex  ">
-                <picture>
-                  <img className="w-[70px] " src={avater} alt={avater} />
-                </picture>
-                <div className="flex justify-center flex-col ml-[14px] ">
-                  <h3 className="font-custom_popins font-semibold text-lg text-black">
-                    Friends Reunion
-                  </h3>
-                  <p className="font-custom_popins font-medium text-sm text-[#4D4D4D]">
-                    Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button className="px-6 bg-primery_Blue text-xl font-custom_popins font-semibold text-white rounded-[5px] ">
-                  Join
-                </button>
-              </div>
-            </div>{" "}
-            {/* profile div  */}
+            ))}
           </div>
         </div>
       </div>
